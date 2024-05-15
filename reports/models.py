ad66e067c20e160e8d36750c09d24cf4893549cc
Ya-Sabyr <sabyrberikuly07@gmail.com>
2024-05-15 16:23:06 +0500

    Model for reports is defined

---
+++ reports/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from sensor.models import Sensor
+
+# Create your models here.
+class Record(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    distance = models.FloatField()
+    time = models.DateTimeField(auto_now_add=True)
+    full = models.BooleanField(default=True)
