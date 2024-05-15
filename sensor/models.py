76dceeef5e6fee6a766f82f253e364b89322ec77
Ya-Sabyr <sabyrberikuly07@gmail.com>
2024-05-15 16:23:51 +0500

    Added default models

--- sensor/models.py
+++ sensor/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class City(models.Model):
     city_name = models.CharField(max_length=100)
-    sensors_number = models.IntegerField()
+    sensors_number = models.IntegerField(default=0)
     
 class District(models.Model):
     district_name = models.CharField(max_length=100)
-    sensors_number = models.IntegerField()
+    sensors_number = models.IntegerField(default=0)
     сity = models.ForeignKey(City, on_delete=models.CASCADE)
 
 """
@@ -18,8 +18,8 @@ class Company(models.Model):
 """
 
 class Sensor(models.Model):
-    created_time = models.TimeField(verbose_name= "Time when sensor was set up" , auto_now_add=True)
-    modified_time = models.TimeField(verbose_name= "Time when something in it was changed")
+    created_at = models.TimeField(verbose_name= "Time when sensor was set up" , auto_now_add=True)
+    modified_at = models.TimeField(verbose_name= "Time when something in it was changed", auto_now=True)
 #    company = models.ForeignKey(Company, on_delete= models.CASCADE, verbose_name= "The sensor it is related to")
     city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name= "The city it is located at")
     district = models.ForeignKey(District, on_delete=models.CASCADE, verbose_name= "The area it is located at")
