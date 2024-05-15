b4109f4c5e8c9ee0fc5b9b4502a0878ff7e797a3
Ya-Sabyr <sabyrberikuly07@gmail.com>
2024-05-15 16:11:08 +0500

    Models for the first time

--- sensor/models.py
+++ sensor/models.py
@@ -3,20 +3,24 @@ from django.db import models
 # Create your models here.
 
 class City(models.Model):
-    city_name = models.CharField(max_length=40)
+    city_name = models.CharField(max_length=100)
     sensors_number = models.IntegerField()
     
 class District(models.Model):
-    district_name = models.CharField(max_length=40)
+    district_name = models.CharField(max_length=100)
     sensors_number = models.IntegerField()
+    сity = models.ForeignKey(City, on_delete=models.CASCADE)
 
+"""
 class Company(models.Model):
-    ...
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+"""
 
 class Sensor(models.Model):
     created_time = models.TimeField(verbose_name= "Time when sensor was set up" , auto_now_add=True)
     modified_time = models.TimeField(verbose_name= "Time when something in it was changed")
-    company = models.ForeignKey(Company, on_delete= models.CASCADE, verbose_name= "The sensor it is related to")
+#    company = models.ForeignKey(Company, on_delete= models.CASCADE, verbose_name= "The sensor it is related to")
     city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name= "The city it is located at")
     district = models.ForeignKey(District, on_delete=models.CASCADE, verbose_name= "The area it is located at")
     address = models.CharField(verbose_name= "The place where it was installed", max_length=100)
