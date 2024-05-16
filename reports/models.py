c83b0683e5646c9c68258191bdaf9710220fa721
Ya-Sabyr <sabyrberikuly07@gmail.com>
2024-05-16 17:27:49 +0500

    Added automatic change of number of full bins. Manager + Proxy

--- reports/models.py
+++ reports/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from sensor.models import Sensor
+from sensor.models import Sensor, City, District
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 # Create your models here.
 class Record(models.Model):
@@ -11,4 +13,35 @@ class Record(models.Model):
     class Meta:
         verbose_name = 'Запись'
         ordering = ['-time']
+
+    def __str__(self):
+        return f"{self.time.strftime('%Y-%m-%d %H:%M:%S')}"
+    
+@receiver(post_save, sender=Record)
+def update_bins_count(sender, instance, **kwargs):
+    if instance.full:
+        instance.sensor.district.full_bins += 1
+        instance.sensor.city.full_bins += 1
+    else:
+        instance.sensor.district.full_bins -= 1
+        instance.sensor.city.full_bins -= 1
+    instance.sensor.district.save()
+    instance.sensor.city.save()
+    
+@receiver(post_delete, sender=Record)
+def delete_bins_count(sender, instance, **kwargs):
+    if instance.full:
+        instance.sensor.district.full_bins -= 1
+        instance.sensor.city.full_bins -= 1
         
+        
+class RecordManager(models.Manager):
+    def get_queryset(self):
+        return super(RecordManager, self).get_queryset().filter(full=True)
+    
+
+class RecordProxy(Record):
+    objects = RecordManager()
+    
+    class Meta:
+        proxy = True
