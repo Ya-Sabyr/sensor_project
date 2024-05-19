bcc25da1273730236453a4eebab6490fdc98b717
Ya-Sabyr <sabyrberikuly07@gmail.com>
2024-05-19 12:24:17 +0500

    Added url to receive data

--- reports/urls.py
+++ reports/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import records_view, record_delete
+from .views import records_view, record_delete, receive_device_data
 
 app_name= 'reports'
 
 urlpatterns = [
     path('', records_view, name='reports'),
     path('delete/<int:record_id>/', record_delete, name='delete'),
+    path('receive-device-data/', receive_device_data, name='receive'),
 ]
     
