c3d253778030f0a63c490ecb2c8d9bb682458420
Ya-Sabyr <sabyrberikuly07@gmail.com>
2024-05-19 12:24:40 +0500

    Views to receive data

--- reports/views.py
+++ reports/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import RecordProxy
+from sensor.models import Sensor
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def records_view(request):
@@ -11,3 +14,22 @@ def record_delete(request, record_id):
         record = RecordProxy.objects.get(id=record_id)
         record.delete()
     return redirect('reports:reports')
+
+@csrf_exempt
+def receive_device_data(request):
+    if request.method == "POST":
+        sensor_id = request.POST.get('sensor_id')
+        distance = request.POST.get('distance')
+        if sensor_id and distance:
+            try:
+                sensor = Sensor.objects.get(id=sensor_id)
+                RecordProxy.objects.create(sensor=sensor, distance=distance, full=True)
+                return JsonResponse({'status': 'success', 'message': 'good boy'}, status=200)
+            except Sensor.DoesNotExist:
+                return JsonResponse({'status': 'error', 'message': 'eshe nemnogo'}, status=404)
+            except ValueError:
+                return JsonResponse({'status': 'error', 'message': 'Value error'}, status=400)
+            except Exception as e:
+                return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
+        return JsonResponse({'status': 'error', 'message': 'Ty dalbayov'}, status=400)
+    return JsonResponse({'status': 'error', 'message': 'Ty pipez tupoy'}, status=405) 
